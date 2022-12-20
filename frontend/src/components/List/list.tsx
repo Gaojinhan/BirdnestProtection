a0fe0809290ab00964ea6364aa85e7869a3e46dd
Jinhan Gao (TAU) <jinhan.gao@tuni.fi>
2022-12-20 23:48:40 +0200

    Add list component

---
+++ frontend/src/components/List/list.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { violationsInfo, violationsInfoList } from '../../types'
+
+const violations: violationsInfoList =  [
+    {
+        firstName: "Dexter",
+        lastName:"Metz",
+        phoneNumber:"+210635740914",
+        createdDt:"2022-05-14T19:40:09.417Z",
+        email:"dexter.metz@example.com"
+    },
+    
+    {
+        firstName: "Darlene",
+        lastName:"Langosh",
+        phoneNumber:"+210569539090",
+        createdDt:"2022-08-23T22:32:06.683Z",
+        email:"darlene.langosh@example.com"
+    }
+]
+
+
+const list = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default list
